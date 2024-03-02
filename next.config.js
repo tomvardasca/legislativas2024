2e55ca2bb4943d50e87ddb953b29d916b2193de9
Tomé Vardasca <tome@vardas.ca>
2024-03-02 02:23:05 +0000

    fix build

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
-const path = require("path");
 /** @type {import('next').NextConfig} */
 module.exports = {
   experimental: {
     serverComponentsExternalPackages: ['llamaindex'],
     // this includes files from the monorepo base two directories up
-    outputFileTracingRoot: path.join(__dirname, 'cache/'),
     outputFileTracingIncludes: {
       '/api/chat': ['./cache/**/*'],
     },
